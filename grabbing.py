2db1bd5c284eef808aa69b492a7ed3687c539f38
Nick Tsai <myintaer@gmail.com>
2023-01-06 16:11:03 +0800

    Add log start time

--- grabbing.py
+++ grabbing.py
@@ -1,6 +1,6 @@
 # Config
-intervalSeconds = 0.2
-durationSeconds = 10
+intervalSeconds = 0.5
+durationSeconds = 5
 cmd = "bash grabbing.sh"
 logFile = "grabbing.log"
 resetLog = True
@@ -12,9 +12,10 @@ import multiprocessing as mp
 from datetime import datetime
 
 def work(num):
+    timeStart = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     output = os.popen(cmd).read()
     time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    log = "\r\nLog by process {} ({}): \r\n".format(num, time) + output.strip() + "\r\n"
+    log = "\r\nLog by process {} ({} - {}): \r\n".format(num, timeStart, time) + output.strip() + "\r\n"
     f = open(logFile, "a")
     f.write(log)
     f.close()
