d0ab654cd9ece1070c6e9ad378d9055ac021fa8d
Nick Tsai <myintaer@gmail.com>
2023-01-08 18:47:04 +0800

    Add countdown function

--- grabbing.py
+++ grabbing.py
@@ -6,22 +6,61 @@ logFile = "grabbing.log"
 resetLog = True
 
 # Main
+import sys
 import os
 import time
 import threading
 from datetime import datetime
 
 def work(num):
-    timeStart = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    timeStart = datetime.now()
+    # Execute command
     output = os.popen(cmd).read()
-    time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    log = "\r\nLog by process {} ({} - {}): \r\n".format(num, timeStart, time) + output.strip() + "\r\n"
+    timeEnd = datetime.now()
+    timeDiff = timeEnd - timeStart
+    log = "\r\nLog by process {} ({} - {} | Total: {}s): \r\n".format(num, timeStart.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3], timeEnd.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3], timeDiff.total_seconds()) + output.strip() + "\r\n"
+    # Write to log
     f = open(logFile, "a")
     f.write(log)
     f.close()
 
 # Main process
 if __name__ == '__main__':
+
+    # Countdown timer
+    if len(sys.argv) > 1:
+        argv1 = sys.argv[1]
+        # print(argv1)
+        targetTimeList = argv1.split(':')
+        # print(targetTimeList)
+
+        # Target time setting
+        targetTime = datetime.now()
+        targetTime = targetTime.replace(microsecond=0)
+        if len(targetTimeList) > 0:
+            targetTime = targetTime.replace(hour=int(targetTimeList[0]))
+        if len(targetTimeList) > 1:
+            targetTime = targetTime.replace(minute=int(targetTimeList[1]))
+        if len(targetTimeList) > 2:
+            targetTime = targetTime.replace(second=int(targetTimeList[2]))
+        # print(targetTime)
+
+        currentTime = datetime.now()
+        print("Current Time: {}\r\nExecution Time: {}".format(currentTime.strftime("%Y-%m-%d %H:%M:%S"), targetTime))
+
+        # Loop checker
+        while targetTime > currentTime:
+            # Diff time
+            timeDiff = targetTime - currentTime
+            diffSecond = timeDiff.seconds
+            if diffSecond < 10 and diffSecond > 0:
+                print("Countdown second: {}".format(diffSecond))
+            # Sleep
+            time.sleep(1)
+            currentTime = datetime.now()
+
+    # Execution
+    print("Begin execution...")
     processNum = int(round(durationSeconds / intervalSeconds))
 
     if resetLog:
