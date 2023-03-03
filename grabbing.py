ddd32b0ad9a0f6f9bf9c3ac67a90d677e1f9734a
Nick Tsai <myintaer@gmail.com>
2023-03-03 11:22:03 +0800

    Optimize

--- grabbing.py
+++ grabbing.py
@@ -1,6 +1,6 @@
 # Config
-intervalSeconds = 0.2
-durationSeconds = 2
+intervalSeconds = 0.1
+durationSeconds = 0.3
 cmd = "bash grabbing.sh"
 logFile = "grabbing.log"
 resetLog = True
@@ -35,10 +35,10 @@ def countDownTimer(timeString: str, renewCount: int = 0):
     targetTime = targetTime.replace(microsecond=0)
     if len(targetTimeList) > 0:
         targetTime = targetTime.replace(hour=int(targetTimeList[0]))
-    if len(targetTimeList) > 1:
-        targetTime = targetTime.replace(minute=int(targetTimeList[1]))
-    if len(targetTimeList) > 2:
-        targetTime = targetTime.replace(second=int(targetTimeList[2]))
+    targetTime = targetTime.replace(minute=int(targetTimeList[1])) if len(targetTimeList) > 1 else targetTime.replace(minute=0)
+    targetTime = targetTime.replace(second=int(targetTimeList[2])) if len(targetTimeList) > 2 else targetTime.replace(second=0)
+    # Diff input
+    targetTime =  targetTime - timedelta(seconds=1)
     # print(targetTime)
 
     # Panel display
@@ -58,18 +58,25 @@ def countDownTimer(timeString: str, renewCount: int = 0):
         # Diff time
         timeDiff = targetTime - currentTime
         diffSecond = timeDiff.seconds
+        # About to start
         if diffSecond < 10 and diffSecond > 0:
-            print("Countdown second: {}".format(diffSecond))
-        # Sleep
-        time.sleep(1)
-        currentTime = datetime.now()
+            if count > 9:
+                print("Countdown second: {}".format(diffSecond))
+                count = 0
+            # Short sleep
+            time.sleep(0.1)
+        else:
+            # Sleep
+            time.sleep(1)
+            if renewCount > 0 and count >= renewCount:
+                output = os.popen(cmd).read()
+                count = 0
+        # Update current timer   
         # Renew
-        count += 1
-        if renewCount > 0 and count >= renewCount:
-            output = os.popen(cmd).read()
-            count = 0
-            # print(output)
-    
+        count += 1     
+        currentTime = datetime.now()
+    # Delay starting based on short sleep for diff input (diff range: 0.1s ~ 0.2s)
+    time.sleep(0.8)
 
 # Main process
 if __name__ == '__main__':
